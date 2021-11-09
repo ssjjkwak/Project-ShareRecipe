f10b217c2871859f8b223df4f2f22c3e88796ac2
jenkwon92 <silverkay92@gmail.com>
2021-11-10 00:52:00 +0900

    11/10 recipe update controller 수정 완료

--- src/main/java/org/kosta/sharecipe/controller/RecipeUpdateController.java
+++ src/main/java/org/kosta/sharecipe/controller/RecipeUpdateController.java
@@ -20,31 +20,73 @@ public class RecipeUpdateController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//int category = Integer.parseInt(request.getParameter("category_num"));
+
+		//프로젝트 완료 후 실제 서비스 시에 사용할 경로 - 프로젝트 새로 로드하면 이미지가 사라지므로 프로젝트 완료후 사용
+		//String saveDir = request.getSession().getServletContext().getRealPath("image");
 		
-		//int no = Integer.parseInt(request.getParameter("no"));
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		//String image = request.getParameter("image");
-		
-		
-		
-		RecipeVO recipeVO = new RecipeVO();
-		recipeVO.setRecipeNo(3);
-		CategoryVO cvo=new CategoryVO(); 
-		cvo.setCategoryNo(1);
-		recipeVO.setCategoryVO(cvo);
-		recipeVO.setTitle(title);
-		recipeVO.setContent(content);
-		//recipeVO.setImage(image);
-		
+		// 이미지 삽입에 관련된 사항들
+		String saveDir="C:/kosta224/toy-project/semi-project-11-8/src/main/webapp/image";
+		//저장될 위치 설정
+		// 정훈
+		//String saveDir = "C:/kosta224/gitTest-workspace/semi-project-11-8/src/main/webapp/image"; // 저장될 위치 설정
+		int maxSize = 3 * 1024 * 1024; // 3M byte 로 크기를 제한
+
+		DiskFileItemFactory itemFactory = new DiskFileItemFactory();
+		itemFactory.setRepository(new File(saveDir));
+		itemFactory.setSizeThreshold(maxSize);
+		itemFactory.setDefaultCharset("utf-8");
+
+		ServletFileUpload upload = new ServletFileUpload(itemFactory); // 위에 설정한 정보를 파일업로드에 넣어줌
+
+		// 업로드된 정보 분석!!! 각각의 컴포넌트들을 FileItem 단위로 쪼갠다..
+		request.setCharacterEncoding("utf-8"); // 다국어 인코딩
+
+		// java.lang.ClassCastException: org.apache.catalina.connector.RequestFacade
+		// cannot be cast to org.apache.tomcat.util.http.fileupload.RequestContext
+		// 이슈 기록대상
+		// List<FileItem> items=upload.parseRequest(request);
+		// List<FileItem> items=upload.parseRequest((RequestContext) request);
+		List<FileItem> items = upload.parseRequest(new ServletRequestContext(request));
+
+		RecipeVO recipeVO = new RecipeVO();// Empty상태의 VO 생성
+
+		for (FileItem item : items) {
+			if (item.isFormField()) { // textfield 라면...db에 넣어야지
+				// vo 에 텍스트필드들의 값을 담자!!
+				if (item.getFieldName().equals("category_num")) {
+					// 카테고리 부분 해결해야함
+					CategoryVO categoryVO = new CategoryVO();
+					int category_num = Integer.parseInt(item.getString());
+					categoryVO.setCategoryNo(category_num);
+					recipeVO.setCategoryVO(categoryVO);
+				} else if (item.getFieldName().equals("no")) {
+					recipeVO.setRecipeNo(Integer.parseInt(item.getString()));
+				} else if (item.getFieldName().equals("title")) {
+					recipeVO.setTitle(item.getString());
+				} else if (item.getFieldName().equals("content")) {
+					recipeVO.setContent(item.getString());
+				}else if (item.getFieldName().equals("id")) { // session 처리 해결 
+					String id = item.getString();
+					MemberVO mvo = new MemberVO(); 
+					mvo.setId(id);
+				}
+			} else { // textfield가 아니라면..업로드 처리
+				String newName = System.currentTimeMillis() + "." + FileManager.getExtend(item.getName()); // png , jpg
+				String destFile = saveDir + "/" + newName; // image/사진1.png
+				File file = new File(destFile);
+				item.write(file);// 물리적 저장 시점
+
+				System.out.println("업로드 완료");
+				recipeVO.setImage(newName);// vo 에 파일명 값을 담아줌
+			}
+		}
 		
 		int result = RecipeDAO.getInstance().updateRecipeByNo(recipeVO);
 		if (result == 0) {
-			System.out.println("등록실패");
+			System.out.println("수정실패");
 			return "redirect:board/recipe-update-fail.jsp";
 		} else {
-			System.out.println("등록성공");
+			System.out.println("수정성공");
 			return "redirect:board/recipe-update-ok.jsp";
 		}
 
