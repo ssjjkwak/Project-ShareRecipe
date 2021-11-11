6141e1ad657e01c0470c6c67b31ca774d038f4c5
anngdung2 <jhsoar1018@gmail.com>
2021-11-11 09:41:04 +0900

    11/11 페이징 작업 추가

--- src/main/java/org/kosta/sharecipe/controller/RecipeListController.java
+++ src/main/java/org/kosta/sharecipe/controller/RecipeListController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.kosta.sharecipe.model.PagingBean;
 import org.kosta.sharecipe.model.RecipeDAO;
 import org.kosta.sharecipe.model.RecipeVO;
 
@@ -12,8 +13,26 @@ public class RecipeListController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ArrayList<RecipeVO> list=RecipeDAO.getInstance().getRecipeList();
+		int totalPostCount=RecipeDAO.getInstance().getTotalPostCount(); //총게시물수 db연동후 받아오기 dao에 메서드있음
+		
+		String pageNo=request.getParameter("pageNo"); //recipe-list.jsp에서 클라이언트가 페이지 조작시 쿼리스트링으로 넘어온다
+		PagingBean pagingBean=null;
+		if(pageNo==null) { //client가 넘긴 페이지. 정보가 없고 그냥 첫화면이면
+			 pagingBean=new PagingBean(totalPostCount);//현재 페이지가 기본 1 page로 할당되어 있음(생성자1개짜리)
+		}else {
+			//client에서 보낸 page번호로 할당한다 //pagingBean생성자2개짜리 보면parameter가 (총게시물수,현재페이지)이다
+			pagingBean=new PagingBean(totalPostCount,Integer.parseInt(pageNo)); 
+			
+		}
+		
+		
+		//ArrayList<RecipeVO> list=RecipeDAO.getInstance().getRecipeList();
+		ArrayList<RecipeVO> list=RecipeDAO.getInstance().getRecipeList(pagingBean);
+		//게시물 리스트 정보
 		request.setAttribute("list", list);
+		//페이징 정보
+		request.setAttribute("pagingBean", pagingBean); //리스트로보낸다
+		
 		return "board/recipe-list.jsp";
 	}
 
--- src/main/webapp/WEB-INF/info/recipetest.sql
+++ src/main/webapp/WEB-INF/info/recipetest.sql
@@ -76,6 +76,68 @@ delete from RECIPE where recipe_num=6;
 --레시피 수정
 update recipe set category_num=2,title='화요일저녁',content='밤',image='1636449018556.jpg' where recipe_num=3;
 
+--페이지구현--
+--데이터삽입
+INSERT INTO recipe(RECIPE_NUM,id,title,content,category_num,image) 
+values(recipe_seq.nextval,'java','페이징테스트1','본문임',1,'1636364271303.jpg');
+INSERT INTO recipe(RECIPE_NUM,id,title,content,category_num,image) 
+values(recipe_seq.nextval,'java','페이징테스트2','본문임',2,'1636364271303.jpg');
+INSERT INTO recipe(RECIPE_NUM,id,title,content,category_num,image) 
+values(recipe_seq.nextval,'java','페이징테스트3','본문임',3,'1636364271303.jpg');
+INSERT INTO recipe(RECIPE_NUM,id,title,content,category_num,image) 
+values(recipe_seq.nextval,'java','페이징테스트4','본문임',4,'1636364271303.jpg');
+
+--배수로 게시물 삽입
+INSERT INTO recipe(RECIPE_NUM,id,title,content,category_num,image) 
+select recipe_seq.nextval,id,title,content,category_num,image from recipe
+
+--너무 많아서 삭제좀 함
+delete from RECIPE where recipe_num between 50 and 100
+
+
+--★★★★★실질적 sql 테스트★★★★
+--rnum test
+select row_number() over(order by RECIPE_NUM desc) as rnum,r.RECIPE_NUM,c.category_name,r.image,r.title,r.id,r.hits,r.likes,r.reg_date 
+from recipe r,category c
+where r.category_num=c.category_num
+
+--진짜연습 step1
+select row_number() over(order by RECIPE_NUM desc) as rnum,RECIPE_NUM,image,title,id,hits,likes,to_char(reg_date,'yyyy.mm.dd')as reg_date
+from recipe
+
+--step2
+select rnum,RECIPE_NUM,image,title,id,hits,likes,reg_date
+from(
+select row_number() over(order by RECIPE_NUM desc) as rnum,RECIPE_NUM,image,title,id,hits,likes,to_char(reg_date,'yyyy.mm.dd')as reg_date
+from recipe
+)where rnum between 1 and 5
+
+--step3
+select r.RECIPE_NUM,c.category_name,r.image,r.title,r.id,r.hits,r.likes,r.reg_date
+from(
+select row_number() over(order by RECIPE_NUM desc) as rnum,RECIPE_NUM,image,title,id,hits,likes,to_char(reg_date,'yyyy.mm.dd')as reg_date
+from recipe
+) r, category c
+where r.category_num=c.category_num and rnum between 1 and 5;
+
+--step4 << 3안되서 새로운방법으로 >>>됐다! (조인먼저 뽑고 그담 rnum 정렬로 뽑은거임)
+select RECIPE_NUM,category_name,image,title,id,hits,likes,reg_date
+from( --카테고리명까지 조인해서 가져온 것으로부터
+select row_number() over(order by RECIPE_NUM desc) as rnum,r.RECIPE_NUM,c.category_name,r.image,r.title,r.id,r.hits,r.likes,r.reg_date 
+from recipe r,category c
+where r.category_num=c.category_num
+)
+where rnum between 1 and 5; --가장최신꺼 5개 불러오겠다
+
+--카테고리로조회 (조인헷갈려서, 그리고 카테고리로 들어온거라 카테고리명은 일단 뺴봄..)
+select RECIPE_NUM,image,title,id,hits,likes,reg_date
+from( 
+select row_number() over(order by RECIPE_NUM desc) as rnum,RECIPE_NUM,image,title,id,hits,likes,reg_date 
+from recipe
+where category_num=1
+)
+where rnum between 1 and 5;
+
 
 
 --데이블 drop 할 경우 아래의 순서로 진행해야함
